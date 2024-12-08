32706a2a11aad3d47ad8ebfd0fb2db07c312b7c6
Axel Aguilar <ajaguilarp@unah.hn>
2024-12-08 14:41:13 -0600

    seleccion de tema

--- tema.php
+++ tema.php
@@ -3,30 +3,31 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: text/html; charset=utf-8');
+obteneeTema();
 
-$tema_id = $_GET['tema_id'];
-context($tema_id);
-
-function context($tema_id){
+function obteneeTema(){
     $host = 'localhost'; // Servidor
     $dbname = 'proyecto_ia'; // Nombre de la base de datos
     $username = 'root'; // Usuario de la base de datos
     $password = 'Lunes08122'; // Contraseña del usuario
     try {
+        // Crear la conexión usando PDO
         $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
-        $sql = "SELECT tema_id,tema_historial FROM tema_historial WHERE tema_id = $tema_id";
-
+        // Escribir la consulta en crudo
+        $sql = "SELECT * FROM tema;";
+        // Ejecutar la consulta
         $stmt = $pdo->query($sql);
     
-       $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        // Recorrer y mostrar los resultados
+       $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
        $respuesta = json_encode(array('data'=> $row));
     
     } catch (PDOException $e) {
+        // Manejo de errores
         $error = "Error: " . $e->getMessage();
         $respuesta = json_encode(array(array('data'=> $error)));
     }
     echo $respuesta;
 }
-
